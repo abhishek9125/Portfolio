2272daef830bf8bfd9093d3815897ee6f6d9ba00
abhishek9125 <abhishek4075@gmail.com>
2024-01-05 23:49:08 +0530

    Added: Skills Section

--- components/About.js
+++ components/About.js
@@ -1,4 +1,5 @@
 import { Briefcase, Calendar, GraduationCap, HomeIcon, MailIcon, PhoneCall, User2 } from 'lucide-react'
+import Image from 'next/image';
 import React from 'react'
 import DevImg from './DevImg';
 import { TabsTrigger } from './ui/tabs';
@@ -243,7 +244,41 @@ function About() {
                                     </div>
                                 </TabsContent>
                                 <TabsContent value="skills">
-                                    Skills Info
+                                    <div className="text-center xl:text-left">
+                                        <h3 className="h3 mb-8">What I Use Everyday</h3>
+                                        <div className="mb-16 ">
+                                            <h4 className="text-xl font-semibold mb-2">Skills</h4>
+                                            <div className="border-b border-border mb-4"></div>
+                                            <div>
+                                                {getData(skillData,'skills').data.map((item, index) => {
+                                                    const { name } = item;
+                                                    return (
+                                                        <div className="w-2/4 text-center xl:text-left mx-auto xl:mx-0" key={index}>
+                                                            <div className="font-medium">{name}</div>
+                                                        </div>
+                                                    )
+                                                })}
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <h4 className="text-xl font-semibold mb-2 xl:text-left">Tools</h4>
+                                            <div className="border-b border-border mb-4"></div>
+                                            <div className="flex gap-x-8 justify-center xl:justify-start">
+                                                {
+                                                    getData(skillData, 'tools').data.map((item, index) => {
+
+                                                        const { imgPath } = item;
+
+                                                        return (
+                                                            <div key={index}>
+                                                                <Image src={imgPath} width={48} height={48} alt="" priority />
+                                                            </div>
+                                                        )
+                                                    })
+                                                }
+                                            </div>    
+                                        </div>
+                                    </div>
                                 </TabsContent>
                             </div>
                         </Tabs>
