7a8d044873935d55d12269bd215e65c59dd9519b
abhishek9125 <abhishek4075@gmail.com>
2024-01-05 23:34:40 +0530

    Added: Personal Info and Qualifications

--- components/About.js
+++ components/About.js
@@ -1,4 +1,4 @@
-import { Calendar, GraduationCap, HomeIcon, MailIcon, PhoneCall, User2 } from 'lucide-react'
+import { Briefcase, Calendar, GraduationCap, HomeIcon, MailIcon, PhoneCall, User2 } from 'lucide-react'
 import React from 'react'
 import DevImg from './DevImg';
 import { TabsTrigger } from './ui/tabs';
@@ -6,104 +6,110 @@ import { TabsContent } from './ui/tabs';
 import { TabsList } from './ui/tabs';
 import { Tabs } from './ui/tabs';
 
+const qualificationData = [
+    {
+        title: 'experience',
+        data: [
+            {
+                company: 'Honasa Consumer Ltd. (MamaEarth)',
+                role: 'Senior Software Engineer',
+                years: 'October 2022 - Present'
+            },
+            {
+                company: 'Honasa Consumer Ltd. (MamaEarth)',
+                role: 'Software Engineer',
+                years: 'July 2021 - September 2022'
+            },
+            {
+                company: 'Honasa Consumer Ltd. (MamaEarth)',
+                role: 'Software Development Intern',
+                years: 'January 2021 - June 2021'
+            }
+        ]
+    },
+    {
+        title: 'education',
+        data: [
+            {
+                university: 'Thapar Institute of Engineering & Technology',
+                qualification: 'Bachelor\'s in Computer Science',
+                years: '2017 - 2021'
+            }
+        ]
+    }
+]
+
+const infoData = [
+    {
+        icon: <User2 size={20} />,
+        text: 'Abhishek Agarwal'
+    },
+    {
+        icon: <PhoneCall size={20} />,
+        text: '+91-7017841375'
+    },
+    {
+        icon: <MailIcon size={20} />,
+        text: 'abhishek4075@gmail.com'
+    },
+    {
+        icon: <Calendar size={20} />,
+        text: 'Born on 25 May, 1998'
+    },
+    {
+        icon: <GraduationCap size={20} />,
+        text: 'Bachelor\'s in Computer Science'
+    },
+    {
+        icon: <HomeIcon size={20} />,
+        text: 'Gurgaon'
+    }
+]
+
+const skillData = [
+    {
+        title: 'skills',
+        data: [
+            {
+                name: 'HTML, CSS'
+            },
+            {
+                name: 'Frontend Development'
+            },
+            {
+                name: 'Javascript, PHP'
+            },
+            {
+                name: 'Backend Development'
+            },
+        ]
+    },
+    {
+        title: 'tools',
+        data: [
+            {
+                imgPath: '/about/vscode.svg'
+            },
+            {
+                imgPath: '/about/figma.svg'
+            },
+            {
+                imgPath: '/about/notion.svg'
+            },
+            {
+                imgPath: '/about/wordpress.svg'
+            },
+        ]
+    }
+];
+
 function About() {
 
-    const qualificationData = [
-        {
-            title: 'experience',
-            data: [
-                {
-                    company: 'Honasa Consumer Ltd. (MamaEarth)',
-                    qualification: 'Software Developer Intern',
-                    years: '2017 - 2021'
-                },
-                {
-                    company: 'Honasa Consumer Ltd. (MamaEarth)',
-                    qualification: 'Software Engineer',
-                    years: '2017 - 2021'
-                },
-                {
-                    company: 'Honasa Consumer Ltd. (MamaEarth)',
-                    qualification: 'Senior Software Engineer',
-                    years: '2017 - 2021'
-                }
-            ]
-        },
-        {
-            title: 'education',
-            data: [
-                {
-                    university: 'Thapar Institute of Engineering & Technology',
-                    qualification: 'Bachelor of Computer Science',
-                    years: '2017 - 2021'
-                }
-            ]
-        }
-    ]
-
-    const infoData = [
-        {
-            icon: <User2 size={20} />,
-            text: 'Abhishek Agarwal'
-        },
-        {
-            icon: <PhoneCall size={20} />,
-            text: '+91-7017841375'
-        },
-        {
-            icon: <MailIcon size={20} />,
-            text: 'abhishek4075@gmail.com'
-        },
-        {
-            icon: <Calendar size={20} />,
-            text: 'Born on 25 May, 1998'
-        },
-        {
-            icon: <GraduationCap size={20} />,
-            text: 'Bachelor\'s in Computer Science'
-        },
-        {
-            icon: <HomeIcon size={20} />,
-            text: 'Gurgaon'
-        }
-    ]
-
-    const skillData = [
-        {
-            title: 'skills',
-            data: [
-                {
-                    name: 'HTML, CSS'
-                },
-                {
-                    name: 'Frontend Development'
-                },
-                {
-                    name: 'Javascript, PHP'
-                },
-                {
-                    name: 'Backend Development'
-                },
-            ]
-        },
-        {
-            title: 'tools',
-            data: [
-                {
-                    imgPath: '/about/vscode.svg'
-                },
-                {
-                    imgPath: '/about/figma.svg'
-                },
-                {
-                    imgPath: '/about/notion.svg'
-                },
-                {
-                    imgPath: '/about/wordpress.svg'
-                },
-            ]
-        }
-    ];
+    const getData = (arr, title) => {
+        return arr.find((item) => item.title === title);
+    }
+    const data = getData(qualificationData, 'experience')
+    console.log(`getData`, data.title)
 
     return (
         <section className="xl:h-[860px] pb-12 py-24">
@@ -113,7 +119,7 @@ function About() {
                 </h2>
                 <div className="flex flex-col xl:flex-row">
                     <div className="hidden xl:flex flex-1 relative">
-                        <DevImg 
+                        <DevImg
                             containerStyles="bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative"
                             imgSrc="/hero/developer2.png"
                             imgStyle="top-[30px] left-[140px]"
@@ -141,7 +147,7 @@ function About() {
                                         <p className="subtitle max-w-xl mx-auto xl:mx-0">
                                             I Specialise in Crafting Intuitive Websites with Cutting Edge Technology, delivering Dynamic and Engaging User Experience
                                         </p>
-                                        <div className="grid xl:grid-cols-2 gap-4">
+                                        <div className="grid xl:grid-cols-2 gap-4 mb-12">
                                             {
                                                 infoData.map((item, index) => {
                                                     return (
@@ -153,10 +159,88 @@ function About() {
                                                 })
                                             }
                                         </div>
+                                        <div className="flex flex-col gap-y-2">
+                                            <div className="text-primary">Language Skills</div>
+                                            <div className="border-b border-border" />
+                                            <div>English, Hindi</div>
+                                        </div>
                                     </div>
                                 </TabsContent>
                                 <TabsContent value="qualifications">
-                                    Qualifications Info
+                                    <div>
+                                        <h3 className="h3 mb-8 text-center xl:text-left">My Awesome Journey</h3>
+                                        <div className="grid md:grid-cols-2 gap-y-8">
+                                            <div className="flex flex-col gap-y-6">
+                                                <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                                                    <Briefcase />
+                                                    <h4 className="capitalize font-medium">
+                                                        {
+                                                            getData(qualificationData, 'experience').title
+                                                        }
+                                                    </h4>
+                                                </div>
+                                                <div className="flex flex-col gap-y-8">
+                                                    {
+                                                        getData(qualificationData, 'experience').data.map((item, index) => {
+                                                            const { company, role, years } = item;
+                                                            return (
+                                                                <div className="flex gap-x-8 group" key={index}>
+                                                                    <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                                                        <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500">
+                                                                        </div>
+                                                                    </div>
+                                                                    <div>
+                                                                        <div className="font-semibold text-xl leading-none mb-2">
+                                                                            {company}
+                                                                        </div>
+                                                                        <div className="text-lg leading-none text-muted-foreground  mb-4">
+                                                                            {role}
+                                                                        </div>
+                                                                        <div className="text-base font-medium">{years}</div>
+                                                                    </div>
+                                                                </div>
+                                                            )
+                                                        })
+                                                    }
+                                                </div>
+                                            </div>
+                                            <div className="flex flex-col gap-y-6">
+                                                <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                                                    <GraduationCap size={28} />
+                                                    <h4 className="capitalize font-medium">
+                                                        {
+                                                            getData(qualificationData, 'education').title
+                                                        }
+                                                    </h4>
+                                                </div>
+                                                <div className="flex flex-col gap-y-8">
+                                                    {
+                                                        getData(qualificationData, 'education').data.map((item, index) => {
+                                                            const { university, qualification, years } = item;
+                                                            return (
+                                                                <div className="flex gap-x-8 group" key={index}>
+                                                                    <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                                                        <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500">
+                                                                        </div>
+                                                                    </div>
+                                                                    <div>
+                                                                        <div className="font-semibold text-xl leading-none mb-2">
+                                                                            {university}
+                                                                        </div>
+                                                                        <div className="text-lg leading-none text-muted-foreground  mb-4">
+                                                                            {qualification}
+                                                                        </div>
+                                                                        <div className="text-base font-medium">{years}</div>
+                                                                    </div>
+                                                                </div>
+                                                            )
+                                                        })
+                                                    }
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
                                 </TabsContent>
                                 <TabsContent value="skills">
                                     Skills Info
