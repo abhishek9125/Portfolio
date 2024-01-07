0c2d75ce41db3b42802c9959b99689dd8ab29935
abhishek9125 <abhishek4075@gmail.com>
2024-01-08 02:32:12 +0530

    Added: Minor Patches

--- components/Form.js
+++ components/Form.js
@@ -1,27 +1,34 @@
 'use client';
 
 import { ArrowRightIcon, MailIcon, MessageSquare, User } from 'lucide-react';
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from './ui/button';
 import { Input } from './ui/input';
 import { Textarea } from './ui/textarea';
 
 function Form() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    
+    console.log(`name`, name)
+
     return (
         <form className="flex flex-col gap-y-4">
             <div className="relative flex items-center">
-                <Input type="name" id="name" placeholder="Name" />
+                <Input type="name" id="name" placeholder="Your Name" value={name} onChange={(e) => setName(e.target.value)} />
                 <User className="absolute right-6" size={20} />
             </div>
 
             <div className="relative flex items-center">
-                <Input type="email" id="email" placeholder="Email" />
-                <MailIcon className="absolute right-6" size={20} />
+                <Input type="email" id="email" placeholder="Your Email" />
+                <MailIcon className="absolute right-6" size={20} value={email} onChange={(e) => setEmail(e.target.value)} />
             </div>
 
             <div className="relative flex items-center">
                 <Textarea placeholder="Type Your Message Here." />
-                <MessageSquare className="absolute top-4 right-6" size={20} />
+                <MessageSquare className="absolute top-4 right-6" value={message} size={20} onChange={(e) => setMessage(e.target.value)} />
             </div>
             <Button className="flex items-center gap-x-1 max-w-[166px]">
                 Let's Talk
