fbac1226e54a8c7e40bb8026659dd5ea65979abc
abhishek9125 <abhishek4075@gmail.com>
2024-01-08 22:11:16 +0530

    Completed: Email Form

--- app/contact/page.js
+++ app/contact/page.js
@@ -12,11 +12,11 @@ function Contact() {
                     <div className="flex flex-col justify-center">
                         <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
                             <span className="w-[30px] h-[2px] bg-primary" ></span>
-                            Say Hello 👋
+                            Say Hi 👋
                         </div>
                         <h1 className="h1 max-w-md mb-8">Let's Work Together.</h1>
                         <p className="subtitle max-w-[400px]">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            You can connect with me in the form below if you want to turn your greatest ideas into reality.
                         </p>
                     </div>
                     <div className="hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat">
--- components/Form.js
+++ components/Form.js
@@ -5,33 +5,104 @@ import React, { useState } from 'react'
 import { Button } from './ui/button';
 import { Input } from './ui/input';
 import { Textarea } from './ui/textarea';
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Form() {
 
+    const [loading, setLoading] = useState(false);
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
-    
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(!email || !name || !message) {
+            toast.error('Please fill the form', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+            return;
+        }
+
+        setLoading(true);
+        const serviceId = "service_j9cviju";
+        const templateId = "template_1uzp3z9";
+        const publicKey = "uTO0U5U4uuQG9v03W"
+
+        const templateParams = {
+            from_name: name,
+            from_email: email,
+            to_name: 'Abhishek from Portfolio',
+            message: `${email} : ${message}`
+        }
+
+        emailjs
+            .send(serviceId, templateId, templateParams, publicKey)
+            .then((response) => {
+                toast.success('🦄 Email Sent Successfully!', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+                console.log('Email Sent Successfully : ', response);
+                setName('');
+                setEmail('');
+                setMessage('');
+            })
+            .catch((error) => {
+                toast.error('Error Sending Email Try Again', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+                console.error('Error Sending Email : ', error);
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+
+    }
+
     return (
-        <form className="flex flex-col gap-y-4">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-y-4">
             <div className="relative flex items-center">
-                <Input type="name" id="name" placeholder="Your Name" value={name} onChange={(e) => setName(e.target.value)} />
+                <Input type="text" id="name" placeholder="Your Name" value={name} onChange={(e) => setName(e.target.value)} />
                 <User className="absolute right-6" size={20} />
             </div>
 
             <div className="relative flex items-center">
-                <Input type="email" id="email" placeholder="Your Email" />
-                <MailIcon className="absolute right-6" size={20} value={email} onChange={(e) => setEmail(e.target.value)} />
+                <Input type="email" id="email" placeholder="Your Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <MailIcon className="absolute right-6" size={20} />
             </div>
 
             <div className="relative flex items-center">
-                <Textarea placeholder="Type Your Message Here." />
-                <MessageSquare className="absolute top-4 right-6" value={message} size={20} onChange={(e) => setMessage(e.target.value)} />
+                <Textarea placeholder="Type Your Message Here." value={message} size={20} onChange={(e) => setMessage(e.target.value)} />
+                <MessageSquare className="absolute top-4 right-6" />
             </div>
-            <Button className="flex items-center gap-x-1 max-w-[166px]">
+            <Button className="flex items-center gap-x-1 max-w-[166px]" disabled={loading}>
                 Let's Talk
                 <ArrowRightIcon size={20} />
             </Button>
+            <ToastContainer />
         </form>
     )
 }
