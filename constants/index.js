b85b523ad60a7e099ab8773f482fbb7020855f4c
abhishek9125 <61241956+abhishek9125@users.noreply.github.com>
2025-03-06 15:27:28 +0530

    Updated: Resume URL

--- constants/index.js
+++ constants/index.js
@@ -50,7 +50,7 @@ export const reviewsData = [
     },
 ]
 
-export const RESUME_LINK = "https://drive.google.com/file/d/16k8mBuwaGFS4KChJeEYuwndMAyWkZnmK/view"
+export const RESUME_LINK = "https://drive.google.com/file/d/1X-_LezNv8XqK_1O3bhPSX0QfNVAmX4K6/view"
 
 export const projectData = [
     {
